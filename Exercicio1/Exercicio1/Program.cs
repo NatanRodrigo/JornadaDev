d4c7f7704b563eb7dafd90073663779e24dacd29
NatanRodrigo <natanrodrigo4@gmail.com>
2025-02-10 19:31:35 -0300

    inclusão vetores e matrizes na segunda LEVA DE exercicios

---
+++ Exercicio1/Exercicio1/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Exercicio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("#######################################");
+            Console.WriteLine("Sistema para calcular médias de provas");
+            Console.WriteLine("#######################################");
+            Console.WriteLine(Environment.NewLine + Environment.NewLine);
+
+            var notas = new List<decimal>();
+            while (true)
+            {
+                Console.WriteLine("Digite uma nota de prova, ou escreva PARE para finalizar o cadastro de notas.");
+                var respostaUsuario = Console.ReadLine();
+
+                if(respostaUsuario.Trim().ToUpper() == "PARE")
+                    break;
+
+                notas.Add(Convert.ToDecimal(respostaUsuario));
+            }
+
+            decimal somaNotas = 0;
+            foreach (var nota in notas)
+            {
+                somaNotas += nota;
+            }
+
+            Console.WriteLine("A média da turma foi: " + somaNotas / notas.Count());
+        }
+    }
+}
